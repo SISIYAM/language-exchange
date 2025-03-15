7aa70ddd0d21b2840e167669e9ad043d5513041d
MD Saymum Islam Siyam <si31siyam@gmail.com>
2025-03-16 00:49:09 +0600

    check pro user while call and learn complete

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-export async function middleware(request) {
+// Middleware to check if the user is authenticated
+async function checkAuthentication(request) {
   const token = request.cookies.get("token")?.value;
 
   if (!token) {
@@ -36,6 +37,66 @@ export async function middleware(request) {
   }
 }
 
+// Middleware to check if the user is subscribed
+async function checkSubscription(request) {
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    console.log("No token found, redirecting to login");
+    const loginUrl = new URL("/login", request.nextUrl.origin);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}/subscription/check-subscription`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      console.error(
+        "API returned an error:",
+        response.status,
+        response.statusText
+      );
+      const subscriptionUrl = new URL("/subscription", request.nextUrl.origin);
+      return NextResponse.redirect(subscriptionUrl);
+    }
+
+    const data = await response.json();
+    console.log("API response data:", data);
+
+    const isSubscribed = data.isSubscribed;
+    console.log("isSubscribed value:", isSubscribed);
+
+    if (!isSubscribed) {
+      console.log("User is not subscribed, redirecting to /subscription");
+      const subscriptionUrl = new URL("/subscription", request.nextUrl.origin);
+      return NextResponse.redirect(subscriptionUrl);
+    }
+
+    console.log("User is subscribed, allowing access");
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Subscription check error:", error);
+    const subscriptionUrl = new URL("/subscription", request.nextUrl.origin);
+    return NextResponse.redirect(subscriptionUrl);
+  }
+}
+
+export async function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  // Apply the subscription check only for the /learning and /call route
+  if (pathname.startsWith("/learning") || pathname.startsWith("/call")) {
+    return checkSubscription(request);
+  }
+
+  // Apply the authentication check for all other routes
+  return checkAuthentication(request);
+}
+
 export const config = {
   matcher: [
     "/profile",
@@ -45,5 +106,6 @@ export const config = {
     "/dashboard",
     "/subscription",
     "/learning",
+    "/call/:path*",
   ],
 };
